c3626488c44ab9c0b6871c16722963949ae2eccc
ankitrustagi3 <ankitgotemail@gmail.com>
2019-07-06 14:34:52 -0700

    Update simple_fs.c

--- simple_fs.c
+++ simple_fs.c
@@ -7,8 +7,8 @@
 #define MAX_FILE_NAME_LENGTH 12
 #define MAX_FD 100
 
-#define MAX_FILE_SIZE 30000
-#define MIN_FILE_SIZE 10000
+#define MAX 30000
+#define MIN 10000
 
 char *rand_name(){
   char fname[MAX_FILE_NAME_LENGTH];
@@ -20,6 +20,37 @@ char *rand_name(){
   
   fname[counter] = '\0';
   return (strdup(fname));
+}
+
+int main(void){
+   int i, j, k;
+   
+   int errorCount = 0;
+   
+   makefs(1);
+   
+   for(i = 0; i < 5; i++){
+      names[i] = rand_name();
+      fds[i] = sfs_open(names[i]);
+      if(fds < 0){
+          fprintf(stderr, "ERROR: creating first test file %s\n", names[i]);
+          errorCount++;
+      }
+      tmp = sfs_open(names[i]);
+      if(tmp >= 0 && fds[i] != tmp){
+          fprintf(stderr, "ERROR: file %s has been opened twice\n", names[i]);
+          errorCount++;
+      }
+      filesize[i] = (rand() % (MAX - MIN)) + MIN;
+   }
+  
+   for(i = 0; i < 5; i++){
+     for(j = i + 1; j < 2; j++){
+        if(fds[i] == fds[j]){
+           fprintf(stderr, "Warning: the file descriptors shouldn't be the same?\n");
+        }
+     }
+   }
 }
 
 
