a5b3d5a15ad86acea67dad4451b2c0f86f7226de
ankitrustagi3 <ankitgotemail@gmail.com>
2019-07-22 00:02:05 -0700

    int opened; 	int wr_ptr; 	int rd_ptr; } FileAccessStatus;
    typedef struct FileDescriptor {
    	char filename[32];
    	int fat_index;
    	time_t timestamp;
    	int size;
    } FileDescriptor;
    
    typedef struct DirectoryDescriptor {
    	FileDescriptor table[max_files];
    	int count;
    } DirectoryDescriptor;
    
    typedef struct fat_node {
    	size_t db_index;
    	int next;
    	int free;
    } fat_node;
    
    typedef struct FileAllocationTable {
    	fat_node table[MAX_DISK];
    	int count;
    } FileAllocationTable;
    
    typedef struct freeblocklist {
    	int list[MAX_DISK];
    } freeblocklist;
    
    typedef union DiskBlock {
    	DirectoryDescriptor dd;
    	FileAllocationTable fat;
    	freeblocklist fbl;
    } DiskBlock;
    
    void mksfs(int fresh);
    void sfs_ls();
    int sfs_open(char * name);
    int sfs_close(int fileID);
    int sfs_write(int fileID, char * buf, int length);
    int sfs_read(int fileID, char* buf, int length);

--- sfs_api.h
+++ sfs_api.h
@@ -7,62 +7,62 @@
 
 DirectoryDescriptor root;
 FileAllocationTable fat;
-
+#Hello There
 static int open_files = 0;
 
 void makefhs(int fresh){
          if(fresh){
                init_fresh_disk("disks.sfs", sizeof(diskBlock), NB_BLOCK);
-               
+
                FAT_init(&fat);
                DirectoryDescriptor_init(&root);
-               
+
                write_blocks(0, 1, (void *) &root);
                write_blocks(1, 1, (void *) &fat);
          }
          else {
-              init_disk("disks.sfs", sizeof(diskBlock), NB_BLOCK); 
+              init_disk("disks.sfs", sizeof(diskBlock), NB_BLOCK);
               read_blocks(0, 1, (void *) &root);
-              read_blocks(1, 1, (void *) &fat);    
+              read_blocks(1, 1, (void *) &fat);
          }
 }
 
 void sfs_ls(){
     printf("\n");
-    
+
     for(int i = 0; i < MAX_FILE; i++){
         if(root.table[i].size > 0){
               int kb = root.table[i].size / 1024;
               char *tm = ctime(&root.table[i].filename);
               tm[24] = '\0';
-              printf("%25s\t%dKB\t%s\n", tm, kb, root.table[i]);   
-        }         
+              printf("%25s\t%dKB\t%s\n", tm, kb, root.table[i]);
+        }
     }
 }
 
 int sfs_open(char * name){
     int fileID = getIndexOfFileInDirectory(name, &root);
-    
+
     if(fileID != -1){
-        return fileID;        
+        return fileID;
     }
-    
+
     fileID = open_files++;
     FileDescriptor_createFiles(name, &(root.table[fileID]));
-    
+
     write_blocks(0, 1, (void *) &root);
     write_blocks(1, 1, (void *) &fat);
-         
-    return fileID;     
+
+    return fileID;
 }
 
 int sfs_close(int fileID){
-    
+
     if(open_files <= fileID){
           fprintf(stderr, "Error: file %d does not exist.", fileID);
-          return -1;   
+          return -1;
     }
-         
+
     root.table[fileID].fas.opened = 0;
     open_files--;
     return 0;
@@ -73,9 +73,9 @@ int sfs_write(int fileID, char * buf, int length){
          fprintf(stderr, "Error: File #%d does not exist. \n", fileID);
          return 0;
     }
-         
+
     int address = 2;
-    
+
     for(int i = 0; i < length; i++){
          if(address != -1){
               write_blocks(address, 1, (void *) &buf[i]);
@@ -86,10 +86,10 @@ int sfs_write(int fileID, char * buf, int length){
               break;
          }
     }
-    
+
     write_blocks(0, 1, (void *) &root);
     write_blocks(1, 1, (void *) &fat)
-         
+
     return root.table[fileID].size;
 }
 
@@ -99,7 +99,7 @@ int sfs_read(int fileID, char * buf, int length){
             return 0;
     }
     buf = FAT_getPartFile(root.table[fileID], fat, length);
-    return EOF;     
+    return EOF;
 }
 
 #endif
