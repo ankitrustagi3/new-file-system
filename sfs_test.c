225eb7049453bfe82e76d28a65380ab96b1dbb51
ankitrustagi3 <ankitgotemail@gmail.com>
2019-07-30 17:08:44 -0700

    Write only

--- sfs_test.c
+++ sfs_test.c
@@ -30,5 +30,46 @@ int main(){
   int chunksize;
   char * buffer;
   int fds[MAX_FD];
-  char * 
+  char * names[MAX_FD];
+  int filesize[MAX_FD];
+  int error_count = 0;
+  int tmp;
+
+  mksfs(1);
+
+  for(i = 0; i < MAX_FILE; i++){
+    names[i] = rand_name();
+    fds[i] = sfs_open(names[i]);
+    if(fds[i] < 0){
+      fprintf(stderr, "The file does not exist!");
+      error_count++;
+    }
+    tmp = open_files(names[i]);
+    if(tmp >= 0 && tmp != fds[i]){
+      fprintf(stderr, "The file does not exist!");
+      error_count++;
+    }
+    filename[i] = (rand() % (MAX_BYTES - MIN_BYTES)) + MIN_BYTES;
+  }
+
+  printf("Opening the five files: \n");
+  sfs_ls();
+  for(i = 0; i < 5; i++){
+    for(j = 0; j < filename[i]; j+=chunksize){
+      if((filename[i] - j) < 10){
+        chunksize = filename[i] - j;
+      }
+      else {
+        chunksize = (rand() % (filesize[i] - j)) + 1;
+      }
+      if((buffer = malloc(chunksize)) == NULL){
+        exit(-1);
+      }
+      for(k = 0; k < chunksize; k++){
+        buffer[k] = (char)(j+k);
+      }
+      sfs_write(fds[i], buffer, chunksize);
+      free(buffer);
+    }
+  }
 }
