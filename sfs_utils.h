ff65c7b52c83ddf4f27e3b010693693042001968
ankitrustagi3 <ankitgotemail@gmail.com>
2019-07-30 17:17:13 -0700

    Create new file

---
+++ sfs_utils.h
@@ -0,0 +1,28 @@
+#ifndef SFS_API_UTIL
+#define SFS_API_UTIL
+
+char vd[100][30000];
+int fatID = 0;
+
+void FAT_init(){
+  
+}
+
+int FAT_getFreeNode(){
+
+}
+
+void FileDescriptor_createFiles(char * name, FileDescriptor file){
+  strcpy(file->filename, name);
+  file->fas.opened = 1;
+  file->fas.rd_ptr = 0;
+  file->fas.wr_ptr = 0;
+  file->size = 0;
+  file->timestamp = time(NULL);
+}
+
+void VirtualDisk_write(int address, void * buff,int length){
+  char * string = (char *) buff;
+  vd[address][length] = '/0';
+  strcpy(string, vd[address], length);
+}
